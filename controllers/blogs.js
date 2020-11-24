e3a97895b4ffbfdb12515294c683ce09bc0930bd
KendaCodes <K.Lamandier@gmail.com>
2020-11-24 17:57:08 -0500

    first commit

---
+++ controllers/blogs.js
@@ -0,0 +1,57 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs.map(blog => blog.toJSON()))
+  })
+})
+
+// blogsRouter.get('/', async (request, response) => {
+//   const blogs = await Blog.find({})
+//   response.json(blogs.map(blog => blog.toJSON()))
+// })
+
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const blog = new Blog({
+    content: body.content,
+    important: body.important === undefined ? false : body.important,
+    date: new Date(),
+  })
+
+  const savedBlog = await blog.save()
+  response.json(savedBlog.toJSON())
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog.toJSON())
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+module.exports = blogsRouter
